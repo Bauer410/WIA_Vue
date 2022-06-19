af69e232ae162edcf33b961f12bd5bfd42caf4e3
stefanpenzinger <stefan.penzinger13@gmail.com>
2022-06-19 13:24:44 +0200

    Added Echse Spock

--- imports/ui/components/UiForm.vue
+++ imports/ui/components/UiForm.vue
@@ -1,4 +1,4 @@
-<template name="ui">
+<template>
     <div id="ui">
       <b-container fluid>
         <b-row>
@@ -6,6 +6,7 @@
             <p v-if="isInGame()">
 
               Status: {{ getStatus() }}
+              ResultMessage: {{ getResultMessage() }}
             </p>
             <b-button v-else type="button" id="play-btn" @click="playButtonClicked">Play</b-button>
           </b-col>
@@ -23,19 +24,27 @@
           </b-col>
         </b-row>
 
-
-
-        <b-row class="selectableField" v-if="isInGame()">
-          <b-col>
-            <b-button type="button" id="Stein" @click="rockClicked">Rock</b-button>
-          </b-col>
-          <b-col>
-            <b-button type="button" id="Papier" @click="paperClicked">Paper</b-button>
-          </b-col>
-          <b-col>
-            <b-button type="button" id="Schere" @click="scissorClicked">Scissor</b-button>
-          </b-col>
-        </b-row>
+        <div v-if="isInGame()">
+          <b-row>
+            <b-col>
+              <b-button type="button" id="Stein" @click="actionClicked">Stein</b-button>
+            </b-col>
+            <b-col>
+              <b-button type="button" id="Papier" @click="actionClicked">Papier</b-button>
+            </b-col>
+            <b-col>
+              <b-button type="button" id="Schere" @click="actionClicked">Schere</b-button>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <b-button type="button" id="Echse" @click="actionClicked">Echse</b-button>
+            </b-col>
+            <b-col>
+              <b-button type="button" id="Spock" @click="actionClicked">Spock</b-button>
+            </b-col>
+          </b-row>
+        </div>
 
         <b-row v-if="finish">
           <b-col>
@@ -49,7 +58,6 @@
 
 <script>
 import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
 import { Session } from 'meteor/session';
 import { Games } from "/lib/games";
 
@@ -59,10 +67,16 @@ export default {
     return {
       username: "",
       password: "",
-      finish: false
+      finish: false,
+      status: "",
+      resultMessage: ""
     };
   },
   methods: {
+    /**
+     * Checks if the player is in a game
+     * @returns {boolean|*} True if in a game, false if not
+     */
     isInGame() {
       let inGame = Session.get("inGame");
       if (typeof inGame === "undefined") {
@@ -70,43 +84,147 @@ export default {
       }
       return Session.get("inGame");
     },
+    /**
+     * Returns the result message
+     * @returns {string} The result message
+     */
+    getResultMessage() {
+      /**
+       * Parses the result message code to a result message
+       * @param resultMessageCode The result message code
+       * @returns {string} The result message
+       */
+      function parseResultMessageCode(resultMessageCode) {
+        let messageCodeSplit = resultMessageCode.split(";");
+        let resultMessage = ""
+
+        if (messageCodeSplit[0] === "Schere") {
+          if (messageCodeSplit[1] === "Stein") {
+            resultMessage = "Stein schleift Schere"
+          }
+          else if (messageCodeSplit[1] === "Spock") {
+            resultMessage = "Spock zerschlägt Schere"
+          }
+          else if (messageCodeSplit[1] === "Papier") {
+            resultMessage = "Schere schneidet Papier"
+          }
+          else if (messageCodeSplit[1] === "Echse") {
+            resultMessage = "Schere schneidet Echse"
+          }
+        }
+        else if (messageCodeSplit[0] === "Stein") {
+          if (messageCodeSplit[1] === "Schere") {
+            resultMessage = "Stein schleift Schere"
+          }
+          else if (messageCodeSplit[1] === "Spock") {
+            resultMessage = "Spock vaporisiert Stein"
+          }
+          else if (messageCodeSplit[1] === "Papier") {
+            resultMessage = "Papier umhüllt Stein"
+          }
+          else if (messageCodeSplit[1] === "Echse") {
+            resultMessage = "Stein zerquetscht Echse"
+          }
+        }
+        else if (messageCodeSplit[0] === "Papier") {
+          if (messageCodeSplit[1] === "Stein") {
+            resultMessage = "Papier umhüllt Stein"
+          }
+          else if (messageCodeSplit[1] === "Echse") {
+            resultMessage = "Echse frisst Papier"
+          }
+          else if (messageCodeSplit[1] === "Schere") {
+            resultMessage = "Schere schneidet Papier"
+          }
+          else if (messageCodeSplit[1] === "Spock") {
+            resultMessage = "Papier entlässt Spock"
+          }
+        }
+        else if (messageCodeSplit[0] === "Echse") {
+          if (messageCodeSplit[1] === "Stein") {
+            resultMessage = "Stein zerquetscht Echse"
+          }
+          else if (messageCodeSplit[1] === "Spock") {
+            resultMessage = "Echse vergiftet Spock"
+          }
+          else if (messageCodeSplit[1] === "Papier") {
+            resultMessage = "Echse frisst Papier"
+          }
+          else if (messageCodeSplit[1] === "Schere") {
+            resultMessage = "Schere schneidet Echse"
+          }
+        }
+        else if (messageCodeSplit[0] === "Spock") {
+          if (messageCodeSplit[1] === "Stein") {
+            resultMessage = "Spock vaporisiert Stein"
+          }
+          else if (messageCodeSplit[1] === "Schere") {
+            resultMessage = "Spock zerschlägt Schere"
+          }
+          else if (messageCodeSplit[1] === "Papier") {
+            resultMessage = "Papier entlässt Spock"
+          }
+          else if (messageCodeSplit[1] === "Echse") {
+            resultMessage = "Echse vergiftet Spock"
+          }
+        }
+
+        this.resultMessage = resultMessage;
+
+        return resultMessage;
+      }
+
+      if (Session.get("inGame")) {
+        let myGame = Games.findOne();
+        let resultMessage = "";
+
+        if (this.status === "Du gewinnst!") {
+          resultMessage = parseResultMessageCode(myGame.resultMessageCode);
+        }
+        else if (this.status === "Du verlierst!") {
+          resultMessage = parseResultMessageCode(myGame.resultMessageCode);
+        }
+
+        return resultMessage
+      }
+    },
+    /**
+     * Returns the current status of the game
+     * @returns {string} The status of the game
+     */
     getStatus() {
         if (Session.get("inGame")) {
             let myGame = Games.findOne();
 
             if(myGame.status === "waiting") {
-              return "Looking for an opponent...";
+              this.status = "Suche nach Gegner....";
             }
             else if(myGame.status === Meteor.userId()) {
-              return "Your turn";
+              this.status = "Du bist am Zug!";
             }
             else if(myGame.status !== Meteor.userId() && myGame.status !== "end") {
-              return "opponent's turn";
+              this.status = "Gegner ist am Zug!";
             }
             else if(myGame.result === Meteor.userId()) {
               this.finish = true
-              return "You won!";
+              this.status = "Du gewinnst!";
             }
             else if(myGame.status === "end" && myGame.result !== Meteor.userId() && myGame.result !== "tie") {
               this.finish = true
-              return "You lost!";
+              this.status = "Du verlierst!";
             }
             else if(myGame.result === "tie") {
               this.finish = true
-              return "It's a tie";
+              this.status = "Unentschieden!";
             }
             else {
-              return "";
+              this.status = "";
             }
+
+            return this.status
         }
     },
-    rockClicked(event) {
-        Meteor.call("games.makeMove", event.target.id);
-    },
-    paperClicked(event) {
-        Meteor.call("games.makeMove", event.target.id);
-    },
-    scissorClicked(event) {
+    actionClicked(event) {
         Meteor.call("games.makeMove", event.target.id);
     },
     playButtonClicked() {
--- server/main.js
+++ server/main.js
@@ -1,22 +1,11 @@
 import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
 import { Games } from "/lib/games";
 
 Meteor.startup(() => {
-  // delete all games at atart
+  // delete all games at start
   Games.remove({});
 
 });
-
-// Meteor.publish('Games', function gamesPublication() {
-// 	return Games.find({ status: "waiting" }, {
-// 		fields: {
-// 			"status": "1",
-// 			"player1": "1",
-// 			"player2": "1"
-// 		}
-// 	});
-// });
 
 Meteor.publish('Games', function gamesPublication() {
   return Games.find({});
